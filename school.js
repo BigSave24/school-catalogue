ca63e07cf35e70d6ead001480066cc73e56a8b24
Kedmond24 <Kedmond24@live.com>
2021-09-06 22:33:35 -0400

    Initial Commit

---
+++ school.js
@@ -0,0 +1,28 @@
+// Parent Class
+class School {
+    constructor(name, level, numberOfStudents){
+        // Complete
+    }
+}
+
+
+class Primary extends School {
+    constructor(){
+        // Complete
+    }
+}
+
+
+class Middle extends School {
+    constructor(){
+        // Complete
+    }
+}
+
+
+class High extends School {
+    constructor(){
+        // Complete
+    }
+}
+
