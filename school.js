c3e11832540f2d700f7ad4ca347c9cb28d78ba58
Kedmond24 <Kedmond24@live.com>
2021-09-11 11:32:40 -0400

    ADD test code for class functionality

--- school.js
+++ school.js
@@ -70,3 +70,18 @@ class HighSchool extends School {
     }
 }
 
+
+
+// Test Class Functionality
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 'primary', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.')
+
+console.log(lorraineHansbury);
+lorraineHansbury.quickFacts()
+console.log(School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']))
+
+
+console.log("\n======================\n")
+
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field'])
+console.log(alSmith)
+alSmith.sportsTeams
